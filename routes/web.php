5f59cf0d109177087016f3f085e8d962b7577829
Nilton Mendes <niltonmendes@Mac-mini-de-Nilton.local>
2025-01-08 12:09:27 -0300

    make a DashboardController exemple, editing route for users and admins, created dashboard views

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'userDashboard'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware(['admin'])->prefix('admin')->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'adminDashboard'])->name('admin.dashboard');
+        // Outras rotas admin aqui
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
